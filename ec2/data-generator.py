ebcd90f07d1f40d51d4b4ecf6f8b00cecc853412
ltdungg <dungluong54321@gmail.com>
2025-04-11 09:08:13 +0700

    EC2 Final

--- ec2/data-generator.py
+++ ec2/data-generator.py
@@ -3,4 +3,104 @@ import json
 import random
 from datetime import datetime
 import pandas as pd
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+import threading
+import time
+
+load_dotenv(".env")
+RDS_HOST = os.getenv("RDS_HOST")
+RDS_PASSWORD = os.getenv("RDS_PASSWORD")
+LAST_ORDER_ID = None
+
+def get_rds_engine():
+    return create_engine(
+        f"postgresql+psycopg2://postgres:{RDS_PASSWORD}@{RDS_HOST}:5432/postgres"
+    )
+
+def get_rds_user_id(engine):
+    with engine.connect() as conn:
+        result = conn.execute(text('SELECT id FROM users;'))
+        user_id = [row[0] for row in result]
+    return user_id
+
+def get_rds_product(engine):
+    with engine.connect() as conn:
+        result = conn.execute(text('SELECT id, price FROM products;'))
+        product_id = [(row[0], row[1]) for row in result]
+    return product_id
+
+def generate_clickstream_data(user_id, product_id):
+    event_type_enum = ['page_view', 'add_to_cart', 'product_view']
+    event_type = random.choice(event_type_enum)
+    clickstream_data = {
+        'user_id': random.choice(user_id),
+        'timestamp': datetime.now(),
+        'event_type': event_type,
+        'product_id': random.choice(product_id) if event_type in ['add_to_cart', 'product_view'] else None
+    }
+    return clickstream_data
+
+def generate_order_data(user_id, products):
+    global LAST_ORDER_ID
+    if LAST_ORDER_ID is None:
+        LAST_ORDER_ID = 0
+    else:
+        LAST_ORDER_ID = LAST_ORDER_ID + 1
+    user_id = random.choice(user_id)
+    order_date = datetime.now()
+
+    numbers_of_product = random.randint(1, 10)
+    order_details = []
+    random_products = random.sample(products, numbers_of_product)
+    total_price = 0
+    for i in range(numbers_of_product):
+        purchase_product = {
+           'order_id': LAST_ORDER_ID,
+           'product_id': random_products[i][0],
+           'quantity': random.randint(1, 5),
+           'unit_price': random_products[i][1]
+        }
+        total_price += random_products[i][1] * purchase_product['quantity']
+        purchase_product_df = pd.DataFrame(purchase_product, index=[0])
+        order_details.append(purchase_product_df)
+    order_data = {
+        'id': LAST_ORDER_ID,
+        'user_id': user_id,
+        'order_date': order_date,
+        'total_price': total_price
+    }
+
+    order_df = pd.DataFrame(order_data, index=[0])
+    order_details_df = pd.concat(order_details, ignore_index=True)
+    return [order_df, order_details_df]
+
+def clickstream_task(engine, user_id, product_id):
+    while True:
+        clickstream_data = generate_clickstream_data(user_id, product_id)
+        print(clickstream_data)
+        time.sleep(random.randint(0, 3))
+
+def order_task(engine, user_id, products):
+    while True:
+        order_data = generate_order_data(user_id, products)
+        order_data[0].to_sql("orders", engine, if_exists="append", index=False)
+        order_data[1].to_sql("order_details", engine, if_exists="append", index=False)
+        print("Order data inserted successfully.")
+        time.sleep(random.randint(0, 3))
+
+if __name__ == "__main__":
+    engine = get_rds_engine()
+    products = get_rds_product(engine)
+    user_id = get_rds_user_id(engine)
+    product_id = [product[0] for product in products]
+
+    # Start clickstream task
+    clickstream_thread = threading.Thread(target=clickstream_task, args=(engine, user_id, product_id))
+    clickstream_thread.start()
+
+    # Start order task
+    order_thread = threading.Thread(target=order_task, args=(engine, user_id, products))
+    order_thread.start()
 
--- ec2/initdb.py
+++ ec2/initdb.py
@@ -1,17 +1,29 @@
+import os
+
 import faker
 import json
 import random
 from datetime import datetime
 import pandas as pd
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
-RDS_HOST = "fashiondb.clm4oskymutz.ap-southeast-1.rds.amazonaws.com"
-RDS_PASSWORD = "Dungluong123"
+load_dotenv(".env")
+
+RDS_HOST = os.getenv("RDS_HOST")
+RDS_PASSWORD = os.getenv("RDS_PASSWORD")
+PRODUCT_PATH = "./product.csv"
+
+def get_rds_engine():
+    return create_engine(
+        f"postgresql+psycopg2://postgres:{RDS_PASSWORD}@{RDS_HOST}:5432/postgres"
+    )
 
 def generate_user_data(num_rows):
     fake = faker.Faker()
     profiles = []
-    for _ in range(num_rows):
+    for i in range(num_rows):
         username = fake.user_name()
         first_name = fake.first_name()
         last_name = fake.last_name()
@@ -22,6 +34,7 @@ def generate_user_data(num_rows):
         sex = fake.random_element(elements=('M', 'F'))
         birthdate = fake.date_of_birth(minimum_age=18, maximum_age=90)
         profile = {
+            "id": i,
             "username": username,
             "first_name": first_name,
             "last_name": last_name,
@@ -42,18 +55,29 @@ def generate_user_data(num_rows):
 def read_product_data(file_path):
     df = pd.read_csv(file_path)
     df = df.drop(columns=["Unnamed: 10"], axis=1)
+    df = df.assign(price=1)
+    df['price'] = df['price'].apply(lambda x: random.randint(1000, 2000))
+
     return df
 
 if __name__ == "__main__":
-    engine = create_engine(
-        f"postgresql+psycopg2://postgres:{RDS_PASSWORD}@{RDS_HOST}:5432/postgres"
-    )
+    engine = get_rds_engine()
+
     # Generate user data
-    user_data = generate_user_data(10)
+    user_data = generate_user_data(1000)
     user_data.to_sql("users", engine, if_exists="replace", index=False)
     print("User data inserted successfully.")
 
+    # Create orders table
+    orders_table = pd.DataFrame(columns=['id', 'user_id', 'order_date', 'total_price'])
+    orders_table.to_sql("orders", engine, if_exists="replace", index=False)
+
+    # Create order details table
+    order_details_table = pd.DataFrame(columns=['order_id', 'product_id', 'quantity', 'unit_price'])
+    order_details_table.to_sql("order_details", engine, if_exists="replace", index=False)
+    print("Orders and order details tables created successfully.")
+
     # Read product data
-    product_data = read_product_data("./product.csv")
+    product_data = read_product_data(PRODUCT_PATH)
     product_data.to_sql("products", engine, if_exists="replace", index=False)
     print("Product data inserted successfully.")
