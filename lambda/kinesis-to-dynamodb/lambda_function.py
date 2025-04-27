323e80c60ababec253fc8115cb08274198db9a7b
ltdungg <dungluong54321@gmail.com>
2025-04-27 16:41:46 +0700

    Feature: kinesis-to-dynamodb done

--- lambda/kinesis-to-dynamodb/lambda_function.py
+++ lambda/kinesis-to-dynamodb/lambda_function.py
@@ -4,6 +4,7 @@ import pandas as pd
 import random
 import base64
 import json
+import awswrangler as wr
 
 # Kinesis
 KINESIS_DATA_STREAM_NAME = os.getenv("KINESIS_DATA_STREAM_NAME")
@@ -16,29 +17,22 @@ dynamodb_client = boto3.client('dynamodb')
 
 # S3
 CLEAN_ZONE_BUCKET = os.getenv("CLEAN_ZONE_BUCKET")
-s3_client = boto3.client('s3')
 
 def get_recommended_products(s3_clean_bucket: str = None, product_id: int = None):
-    response = s3_client.get_object(
-        Bucket=s3_clean_bucket,
-        Key=f'products/products.parquet'
-    )
-    parquet_file = response["Body"]
-    products_df = pd.read_parquet(path=parquet_file)
+    products_df = wr.s3.read_parquet(path=f"s3://{s3_clean_bucket}/products/products.parquet")
 
-    product_index = products_df[products_df['id'] == product_id].index
+    product_index = products_df[products_df['id'] == product_id].index[0]
     sub_category = products_df.at[product_index, 'sub_category']
 
     recommended_products = products_df[products_df['sub_category'] == sub_category]['id'].to_list()
 
-    numbers_recommend = random.randint(1, len(recommended_products)-1)
-    print(f"Successfully put recommended products for {product_id}")
+    numbers_recommend = random.randint(1, (len(recommended_products)-1) % 5)
     return random.sample(recommended_products, numbers_recommend)
 
 def push_to_dynamodb(recommended_products: list = None, dynamo_table: str = None, user_id: int = None):
     recommended_dict = {
-        'user_id': user_id,
-        'recommended_products': recommended_products
+        'user_id': {'N': f'{user_id}'},
+        'recommended_products': {'L': [{'N': f'{x}'} for x in recommended_products]}
     }
 
     response = dynamodb_client.put_item(
@@ -53,14 +47,13 @@ def lambda_handler(event, context):
     response = event['Records'][0]
     try:
         print(f"Processed Kinesis Event - EventID: {response['eventID']}")
-        record_data_decode = base64.b64decode(response['kinesis']['data']).decode('utf-8').decode()
-        decoded_record_data = base64.b64decode(record_data_decode).decode()
-        print(f"Record Data: {decoded_record_data}")
+        record_data = base64.b64decode(response['kinesis']['data']).decode('utf-8')
+        print(f"Record Data: {record_data}")
     except Exception as e:
         print(f"An error occurred {e}")
         raise e
 
-    data = json.loads(decoded_record_data)
+    data = json.loads(record_data)
 
     if data['event_type'] in ['add_to_cart', 'product_view']:
         product_id = data['product_id']
