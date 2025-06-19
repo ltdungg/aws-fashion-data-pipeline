d33ff6651b4a4fbd814d6e04a508272db8cba5a9
ltdungg <dungluong54321@gmail.com>
2025-06-19 22:01:12 +0700

    Fix: kinesis-to-dynamo code

--- lambda/kinesis-to-dynamodb/lambda_function.py
+++ lambda/kinesis-to-dynamodb/lambda_function.py
@@ -15,18 +15,18 @@ kinesis_client = boto3.client('kinesis')
 TABLE_NAME = os.getenv("DYNAMODB_TABLE_NAME")
 dynamodb_client = boto3.client('dynamodb')
 
-# S3
-CLEAN_ZONE_BUCKET = os.getenv("CLEAN_ZONE_BUCKET")
+# Catalog
+CLEAN_ZONE_DATABASE_CATALOG = os.getenv("CLEAN_ZONE_DATABASE_CATALOG")
 
-def get_recommended_products(s3_clean_bucket: str = None, product_id: int = None):
-    products_df = wr.s3.read_parquet(path=f"s3://{s3_clean_bucket}/products/")
+def get_recommended_products(clean_zone_catalog: str = None, product_id: int = None):
+    products_df = wr.athena.read_sql_query("SELECT * FROM products", database=clean_zone_catalog)
 
-    product_index = products_df[products_df['id'] == product_id].index[0]
+    product_index = products_df[products_df['product_id'] == product_id].index[0]
     sub_category = products_df.at[product_index, 'sub_category']
 
-    recommended_products = products_df[products_df['sub_category'] == sub_category]['id'].to_list()
+    recommended_products = products_df[products_df['sub_category'] == sub_category]['product_id'].to_list()
 
-    numbers_recommend = random.randint(1, (len(recommended_products)-1) % 5)
+    numbers_recommend = random.randint(1, len(recommended_products) % 5)
     return random.sample(recommended_products, numbers_recommend)
 
 def push_to_dynamodb(recommended_products: list = None, dynamo_table: str = None, user_id: int = None):
@@ -47,7 +47,7 @@ def lambda_handler(event, context):
     response = event['Records'][0]
     try:
         print(f"Processed Kinesis Event - EventID: {response['eventID']}")
-        record_data = response['kinesis']['data'].decode('utf-8')
+        record_data = base64.b64decode(response['kinesis']['data'])
         print(f"Record Data: {record_data}")
     except Exception as e:
         print(f"An error occurred {e}")
@@ -59,7 +59,7 @@ def lambda_handler(event, context):
         product_id = data['product_id']
         user_id = data['user_id']
 
-        recommended_products = get_recommended_products(s3_clean_bucket=CLEAN_ZONE_BUCKET,
+        recommended_products = get_recommended_products(clean_zone_catalog=CLEAN_ZONE_DATABASE_CATALOG,
                                                         product_id=product_id)
 
         status = push_to_dynamodb(recommended_products=recommended_products,
